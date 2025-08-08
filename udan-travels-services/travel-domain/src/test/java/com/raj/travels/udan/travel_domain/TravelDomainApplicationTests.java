cafc7dd0ab5beda3baeade0adc4409f40131adc8
Yubraj-Sahoo <157888387+Yubraj-Sahoo@users.noreply.github.com>
2025-08-08 23:33:35 +0530

    "impl-connection : create modules for travel-domain and connection-service"

---
+++ udan-travels-services/travel-domain/src/test/java/com/raj/travels/udan/travel_domain/TravelDomainApplicationTests.java
@@ -0,0 +1,13 @@
+package com.raj.travels.udan.travel_domain;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TravelDomainApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
